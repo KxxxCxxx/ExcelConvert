c068ae5d2bcc2ca26f60e1e2fed84d88c7460e56
KxxxCxxx <soonchul13@naver.com>
2021-06-16 17:21:26 +0900

    First

---
+++ ExcelConvert/Form1.Designer.cs
@@ -0,0 +1,320 @@
+﻿namespace ExcelConvert
+{
+    partial class ExcelConvert
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.btnExcelSave = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column10 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column11 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column12 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column13 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column14 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column15 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column16 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column17 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column18 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column19 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column20 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column21 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column22 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column23 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column24 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column25 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column26 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.btnExcelSave);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.dataGridView1);
+            this.splitContainer1.Size = new System.Drawing.Size(1804, 956);
+            this.splitContainer1.SplitterDistance = 41;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // btnExcelSave
+            // 
+            this.btnExcelSave.Location = new System.Drawing.Point(12, 3);
+            this.btnExcelSave.Name = "btnExcelSave";
+            this.btnExcelSave.Size = new System.Drawing.Size(84, 35);
+            this.btnExcelSave.TabIndex = 0;
+            this.btnExcelSave.Text = "엑셀 저장";
+            this.btnExcelSave.UseVisualStyleBackColor = true;
+            this.btnExcelSave.Click += new System.EventHandler(this.btnExcelSave_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.Column7,
+            this.Column8,
+            this.Column9,
+            this.Column10,
+            this.Column11,
+            this.Column12,
+            this.Column13,
+            this.Column14,
+            this.Column15,
+            this.Column16,
+            this.Column17,
+            this.Column18,
+            this.Column19,
+            this.Column20,
+            this.Column21,
+            this.Column22,
+            this.Column23,
+            this.Column24,
+            this.Column25,
+            this.Column26});
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(1804, 911);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.DragDrop += new System.Windows.Forms.DragEventHandler(this.dataGridView1_DragDrop);
+            this.dataGridView1.DragEnter += new System.Windows.Forms.DragEventHandler(this.dataGridView1_DragEnter);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "묶음값";
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 70;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "배송지";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "성명(한글)";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "성명(영어)";
+            this.Column4.Name = "Column4";
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "개인통관번호";
+            this.Column5.Name = "Column5";
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "빈값(고정)";
+            this.Column6.Name = "Column6";
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "핸드폰";
+            this.Column7.Name = "Column7";
+            // 
+            // Column8
+            // 
+            this.Column8.HeaderText = "용도구분";
+            this.Column8.Name = "Column8";
+            // 
+            // Column9
+            // 
+            this.Column9.HeaderText = "우편번호";
+            this.Column9.Name = "Column9";
+            // 
+            // Column10
+            // 
+            this.Column10.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Column10.HeaderText = "주소";
+            this.Column10.Name = "Column10";
+            this.Column10.Width = 51;
+            // 
+            // Column11
+            // 
+            this.Column11.HeaderText = "상세주소";
+            this.Column11.Name = "Column11";
+            // 
+            // Column12
+            // 
+            this.Column12.HeaderText = "영문주소";
+            this.Column12.Name = "Column12";
+            // 
+            // Column13
+            // 
+            this.Column13.HeaderText = "배송시요청사항";
+            this.Column13.Name = "Column13";
+            // 
+            // Column14
+            // 
+            this.Column14.HeaderText = "주문번호(Order No)";
+            this.Column14.Name = "Column14";
+            // 
+            // Column15
+            // 
+            this.Column15.HeaderText = "상품명(영문)";
+            this.Column15.Name = "Column15";
+            // 
+            // Column16
+            // 
+            this.Column16.HeaderText = "색상";
+            this.Column16.Name = "Column16";
+            // 
+            // Column17
+            // 
+            this.Column17.HeaderText = "사이즈";
+            this.Column17.Name = "Column17";
+            // 
+            // Column18
+            // 
+            this.Column18.HeaderText = "수량";
+            this.Column18.Name = "Column18";
+            // 
+            // Column19
+            // 
+            this.Column19.HeaderText = "단가";
+            this.Column19.Name = "Column19";
+            // 
+            // Column20
+            // 
+            this.Column20.HeaderText = "이미지 URL";
+            this.Column20.Name = "Column20";
+            // 
+            // Column21
+            // 
+            this.Column21.HeaderText = "상품 URL";
+            this.Column21.Name = "Column21";
+            // 
+            // Column22
+            // 
+            this.Column22.HeaderText = "TRACKING#";
+            this.Column22.Name = "Column22";
+            // 
+            // Column23
+            // 
+            this.Column23.HeaderText = "물류센터 요청사항";
+            this.Column23.Name = "Column23";
+            // 
+            // Column24
+            // 
+            this.Column24.HeaderText = "품목번호";
+            this.Column24.Name = "Column24";
+            // 
+            // Column25
+            // 
+            this.Column25.HeaderText = "부가서비스";
+            this.Column25.Name = "Column25";
+            // 
+            // Column26
+            // 
+            this.Column26.HeaderText = "재고번호";
+            this.Column26.Name = "Column26";
+            // 
+            // ExcelConvert
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1804, 956);
+            this.Controls.Add(this.splitContainer1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "ExcelConvert";
+            this.Text = "Main";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column9;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column10;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column11;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column12;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column13;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column14;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column15;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column16;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column17;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column18;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column19;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column20;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column21;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column22;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column23;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column24;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column25;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column26;
+        private System.Windows.Forms.Button btnExcelSave;
+    }
+}
+
---
+++ ExcelConvert/Form1.cs
@@ -0,0 +1,211 @@
+﻿using Microsoft.Office.Interop.Excel;
+using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace ExcelConvert
+{
+    public partial class ExcelConvert : Form
+    {
+        string gFilePath = "";
+        public ExcelConvert()
+        {
+            InitializeComponent();
+
+            dataGridView1.AllowDrop = true;
+
+            //this.dataGridView1.DragDrop += new System.Windows.Forms.DragEventHandler(this.dataGridView1_DragDrop);
+        }
+
+        private void dataGridView1_DragDrop(object sender, DragEventArgs e)
+        {
+            gFilePath = "";
+
+            if (e.Data.GetDataPresent(DataFormats.FileDrop)) {
+                e.Effect = DragDropEffects.Copy;
+                string[] filepath = (string[]) e.Data.GetData(DataFormats.FileDrop);
+                gFilePath = filepath[0];
+
+                //MessageBox.Show(System.Windows.Forms.Application.StartupPath);
+                //MessageBox.Show( System.Security.Principal.WindowsIdentity.GetCurrent().Name);
+                if (!gFilePath.Equals("") && true)
+                {
+                    fExcelLoad();
+                }
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+
+            //MessageBox.Show(gFilePath);
+
+        }
+
+        private void fExcelSave()
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.ShowDialog();
+            string select_path = dialog.SelectedPath;
+
+            if (String.IsNullOrEmpty(select_path))
+            {
+                return;
+            }
+
+            Microsoft.Office.Interop.Excel.Application application = null;
+            Workbook workbook = null;
+            Worksheet worksheet = null;
+            Range range = null;
+
+            try
+            {
+                string sRootPath = System.Windows.Forms.Application.StartupPath;
+
+                application = new Microsoft.Office.Interop.Excel.Application();
+                workbook = application.Workbooks.Open(sRootPath + "\\" + "주문.xls");
+                worksheet = workbook.Worksheets.get_Item(1);
+
+                application.Visible = false;
+
+                range = worksheet.UsedRange;
+
+                for (int i = 0; i < dataGridView1.Rows.Count; i++)
+                {
+                    for(int j = 0; j < dataGridView1.Rows[i].Cells.Count; j++)
+                    {
+                        range[i + 2, j + 1] = String.IsNullOrEmpty((string) dataGridView1.Rows[i].Cells[j].Value) ? "" : dataGridView1.Rows[i].Cells[j].Value.ToString();
+                    }
+
+                }
+
+                worksheet.SaveAs(select_path + "\\" + "주문_" + DateTime.Now.ToString("yyyyMMddhhmmss"));
+
+                DeleteObject(worksheet);
+                DeleteObject(workbook);
+                application.Quit();
+                DeleteObject(application);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void fExcelLoad()
+        {
+            Microsoft.Office.Interop.Excel.Application application = null;
+            Workbook workbook = null;
+            Worksheet worksheet = null;
+            Range range = null;
+
+            try
+            {
+                application = new Microsoft.Office.Interop.Excel.Application();
+                workbook = application.Workbooks.Open(gFilePath);
+                worksheet = workbook.Worksheets.get_Item(1);
+
+                application.Visible = false;
+
+                range = worksheet.UsedRange;
+
+                dataGridView1.Rows.Clear();
+
+                int idx = -1;
+
+                for (int i = 2; i <= range.Rows.Count; ++i)
+                {
+                    dataGridView1.Rows.Add();
+                    idx++;
+
+                    dataGridView1.Rows[idx].Cells[1].Value = "항공(인천)"; //상세주소
+                    dataGridView1.Rows[idx].Cells[7].Value = "개인"; //용도구분
+                    dataGridView1.Rows[idx].Cells[10].Value = "1"; //상세주소
+                    dataGridView1.Rows[idx].Cells[23].Value = "115"; //상세주소
+
+                    for (int j = 1; j <= range.Columns.Count; ++j)
+                    {
+                        switch (j)
+                        {
+                            case 25: //구매자
+                                dataGridView1.Rows[idx].Cells[2].Value = (range.Cells[i, j] as Range).Value2.ToString();
+                                break;
+                            case 36: //개인통관번호
+                                dataGridView1.Rows[idx].Cells[4].Value = (range.Cells[i, j] as Range).Value2.ToString();
+                                break;
+                            case 37: //구매자 핸드폰번호
+                                dataGridView1.Rows[idx].Cells[6].Value = (range.Cells[i, j] as Range).Value2.ToString();
+                                break;
+                            case 29: //우편번호
+                                dataGridView1.Rows[idx].Cells[8].Value = (range.Cells[i, j] as Range).Value2.ToString();
+                                break;
+                            case 30: //주소
+                                dataGridView1.Rows[idx].Cells[9].Value = (range.Cells[i, j] as Range).Value2.ToString();
+                                break;
+                            case 23: //구매수량
+                                dataGridView1.Rows[idx].Cells[17].Value = (range.Cells[i, j] as Range).Value2.ToString();
+                                break;
+                            case 13: //노출상품명 -> 영문주소
+                                dataGridView1.Rows[idx].Cells[11].Value = (range.Cells[i, j] as Range).Value2.ToString();
+                                break;
+                            case 14: //노출상품ID -> 배송시요청사항
+                                dataGridView1.Rows[idx].Cells[12].Value = (range.Cells[i, j] as Range).Value2.ToString();
+                                break;
+                            case 19: //결제액 -> 색상
+                                dataGridView1.Rows[idx].Cells[15].Value = (range.Cells[i, j] as Range).Value2.ToString();
+                                break;
+                            default:
+                                break;
+                        }
+
+                    }
+                }
+
+                //range.Cells[3, 3] = "123";
+                //worksheet.SaveAs(@"C:\Users\Kwon\Desktop\123.xlsx");
+
+                DeleteObject(worksheet);
+                DeleteObject(workbook);
+                application.Quit();
+                DeleteObject(application);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            
+        }
+
+        private void DeleteObject(object obj)
+        {
+            try
+            {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(obj);
+                obj = null;
+            }
+            catch (Exception ex)
+            {
+                obj = null;
+                MessageBox.Show("메모리 할당을 해제하는 중 문제가 발생하였습니다." + ex.ToString(), "경고!");
+            }
+            finally
+            {
+                GC.Collect();
+            }
+        }
+
+        private void dataGridView1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.All;
+            else
+                e.Effect = DragDropEffects.None;
+        }
+
+        private void btnExcelSave_Click(object sender, EventArgs e)
+        {
+            fExcelSave();
+        }
+    }
+}
